a668f359c0f429af38b3f26d4fbd76ca4f64a2fc
PATX0 <tiagovcsoares@gmail.com>
2018-10-23 20:01:57 +0100

    board_moves minimamente funcional

--- proj1.py
+++ proj1.py
@@ -50,28 +50,55 @@ def move_final(move):
 	return move[1]
 
 # board_moves(board): Movimentos possiveis no tabuleiro
+
 def board_moves(board):
 	n_l = len(board)
 	n_c = len(board[0])
-	list_pos = []
-	# generate the mapping of the board as positions in list_pos
-	for l in range(0, n_l):
-		for c in range(0, n_c):
-			position = make_pos(l, c)
-			list_pos.append(position)
-	#verificar posicoes possiveis,(?) encontrar posicoes que tenham na vizinhanca uma ou + posicoes vazias
-	# if (make_pos(pos_l(position)+1, pos_c(position))) in list_pos:
-	# if (make_pos(pos_l(position), pos_c(position)+1))) in list_pos:
-	# if (make_pos(pos_l(position)-1, pos_c(position))) in list_pos:
-	# if (make_pos(pos_l(position), pos_c(position)-1))) in list_pos:
-	return list_pos
+	moves = []
+	positions = map_positions_board(n_l, n_c, board) #lista de posicoes do board
+	for pos in positions:
+		l = pos_l(pos)
+		c = pos_c(pos)
+		if is_empty(board[l][c]): 
+			neigh_d2 = getNeighbours(pos, board) #lista de vizinhos de pos com dist = 2
+			for i in range(0, len(neigh_d2)):
+				aux2_l = pos_l(neigh_d2[i])
+				aux2_c = pos_c(neigh_d2[i])
+				if is_peg(board[aux2_l][aux2_c]):
+					if aux2_l < l:	#verifica se a peca a mover esta a esquerda da posicao vazia 
+						if is_peg(board[l-1][c]): #verifica se a posicao entre ambas e uma peca valida
+							moves.append(make_move(neigh_d2[i], pos))
+
+					if aux2_l > l:	#verifica se a peca a mover esta a direita da posicao vazia
+						if is_peg(board[l+1][c]):
+							moves.append(make_move(neigh_d2[i], pos))
+
+					if aux2_c < c:	#verifica se a peca a mover esta acima da posicao vazia
+						if is_peg(board[l][c-1]):
+							moves.append(make_move(neigh_d2[i], pos))
+
+					if aux2_c > c: #verifica se a posicao a mover esta abaixo da posicao vazia
+						if is_peg(board[l][c+1]):
+							moves.append(make_move(neigh_d2[i], pos))
+
+	return moves
+
 
 # board_perform_move(board): Movimentos possiveis no tabuleiro
 def board_perform_move(board, move):
 	board_cpy = deepcopy(board)
 	return board_cpy
 
+#def check_return_empty(list_pos, board):
+#	res = []
+#	for i in range(0, len(list_pos)):
+#		l = pos_l(list_pos[i])
+#		c = pos_c(list_pos[i])
+#		if is_empty(board[l][c]):
+#			res.append(list_pos[i])
+#	return res
 
+	
 def countPegs(board):
 	count = 0
 	nr_l = len(board)
@@ -92,6 +119,42 @@ def countEmpty(board):
 				count += 1
 	return count
 
+def getNeighbours(pos, board):
+	n_ls = len(board)
+	n_cs = len(board[0])   
+	l = pos_l(pos)
+	c = pos_c(pos)
+	#res1 = []
+	res2 = []
+	#if l > 0: 		
+	#	res1.append(make_pos(l-1, c))
+	#if l < n_ls-1:
+	#	res1.append(make_pos(l+1, c))    
+	#if c > 0:
+	#	res1.append(make_pos(l, c-1))
+	#if c < n_cs-1:
+	#	res1.append(make_pos(l, c+1))
+	if l > 1: 		
+		res2.append(make_pos(l-2, c))
+	if l < n_ls-2:
+		res2.append(make_pos(l+2, c))    
+	if c > 1:
+		res2.append(make_pos(l, c-2))
+	if c < n_cs-2:
+		res2.append(make_pos(l, c+2))
+	
+	return sorted(res2)
+
+
+
+def map_positions_board(n_l, n_c, board):
+	list_pos = []
+	# generate the mapping of the board as positions in list_pos
+	for l in range(0, n_l):
+		for c in range(0, n_c):
+			position = make_pos(l, c)
+			list_pos.append(position)
+	return list_pos
 
 class sol_state:
 
@@ -101,18 +164,22 @@ class sol_state:
 	def __lt__(self, other):
 		return len(board_moves(self.board)) > len(board_moves(other.board))
 
+
+
 class solitaire(Problem):
 #Models a Solitaire problem as a satisfaction problem.
 #A solution cannot have more than 1 peg left on the board.
 	def __init__(self, board):
 		self.initial = sol_state(board)
 
-	#def actions(self, state):
-	#	result = board_moves(state.board)
+	def actions(self, state):
+		result = board_moves(state.board)
+		return result 
 
 	def result(self, state, action): #executar move(action) no estado e devolver o novo estado
-		return board_perfome_move(state.board, action)
-
+		res = board_perfome_move(state.board, action)
+		return sol_state(res)
+		
 	def goal_test(self, state): #state e solucao se so tiver 1 peca
 		return countPegs(state.board) == 1
 
